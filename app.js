c74ca9a89e5212c6585f538a0f89dc2446ba142a
Trenton Creamer <trentoncreamer@yahoo.com>
2023-06-13 05:07:52 -0700

    cors

--- app.js
+++ app.js
@@ -18,10 +18,22 @@ app.use(
 app.use(
   cors({
     credentials: true,
-    // origin: "http://localhost:3000",
-    origin: "*",
+    // origin: "*",
+    origin: "http://localhost:3000",
   })
 );
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
+
 app.use(express.json());
 
 TuitController(app);
